4f28ae733c33ee7e89abd54d28548c3ab3a0c83a
shayn-234 <shayanshakeel801@gmail.com>
2025-12-28 00:42:27 +0500

    first project

---
+++ blank-canvas.py
@@ -0,0 +1,16 @@
+import turtle
+t = turtle.Turtle()
+def move():
+    t.forward(20)
+def turn_left():
+    t.left(20)
+def move2():
+    t.back(20)
+def turn_right():
+    t.right(20)
+turtle.listen()
+turtle.onkeypress(move2, 'Down')
+turtle.onkeypress(move, 'Up')
+turtle.onkeypress(turn_left, 'Left')
+turtle.onkeypress(turn_right, 'Right')
+turtle.done()
