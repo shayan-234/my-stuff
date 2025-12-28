3dcb07b5eaf00c5c88b94da6134f4eb52068ad45
shayn-234 <shayanshakeel801@gmail.com>
2025-12-28 22:28:40 +0500

    my folder for practicing

--- phyton/test.py
+++ phyton/test.py
@@ -1,5 +1,24 @@
 import turtle
 t = turtle.Turtle()
+wn = turtle.Screen()
+wn.setup(width = 400, height= 600)
+wn.title("hello")
+wn.bgcolor("orange")
+text = turtle.Turtle()
+text.hideturtle()
+text.penup()
+text.goto(0, -50)
+text.color("black")
+text.write("button", align="center", font=("Arial", 20, 'bold'))
+t.penup()
+t.goto(0, -50)
+t.shape("square")
+t.shapesize(stretch_wid=2, stretch_len=6)
+t.color("blue")
+def clicked(a, b):
+    text.write("clicked", align="center")
+t.onclick(clicked)
+
 t.pensize(40)
 def move():
     t.forward(20)
